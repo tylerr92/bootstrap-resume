c1d1f9b3abf2ec4455c705edce66ed4ce3aa3623
Tyler Radlick <tyler@Tylers-iMac-Pro.local>
2018-10-04 19:55:15 -0500

    Removing Mobile Menu Icon

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
   <div class="glass"></div>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+  <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
-  </button>
+  </button> -->
   <div class="collapse navbar-collapse" id="navbarCollapse">
     <div class="row mx-auto">
       <div class="col-12 text-light text-center">
