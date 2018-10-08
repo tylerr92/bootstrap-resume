fe9d070ca491e5c328d985ac05ed2c83f712433a
Tyler Radlick <tyler@Tylers-iMac-Pro.local>
2018-10-08 00:39:56 -0500

    Updated Footer, Added Github Collab templates, Corrected readme file.

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="resume-footer mt-4">
-  <p>Designed with <i class="fas fa-heart"></i> by Tyler J. Radlick. All Rights Reserved. &copy; 2018 Tyler J. Radlick</p>
+  <p>Designed with <i class="fas fa-heart"></i> by Tyler J. Radlick. All Rights Reserved. Version 1.0 &copy; 2018 Tyler J. Radlick</p>
   <p>
     <a href="#">Back to top</a>
   </p>
